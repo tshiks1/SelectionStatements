d0e5f2cc63e614df42a621285db16c30f255fd88
Teresia Ngigi <tshiks22@outlook.com>
2020-10-26 22:47:24 -0500

    attempt 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int favNumber = 89;
+
+            if (favNumber <= 88)
+            {
+                Console.WriteLine("Too low");
+            }
+
+            else if (89 == 89)
+            {
+                Console.WriteLine("You are Right");
+            }
+            else
+            {
+                Console.Write("Nevermind");
+            }
+
+            //Create an if-statement that if the guessed number is below the initial value, print out “too low”.
+           // Create an else -if statement that if the number is higher than the initial value, print out “too high”.
+//Create an else statement that prints out “Nevermind”.
+
+
+
+
+
+           
         }
     }
 }
